d2f8845f6cea743c75c5d1cfb30681c2226eef53
Tanmay Patel <tanmay.1rivet@gmail.com>
2023-02-21 14:48:04 +0530

    changes

--- src/components/todoTaskDEtails.ts
+++ src/components/todoTaskDEtails.ts
@@ -3,15 +3,3 @@ export interface Todos {
     todo: string,
     isCompleted: boolean
 }
-// export const todoList: Todo[] = [
-//     {
-//         id: 1,
-//         todo: "Completed online JavaScript courese",
-//         isCompleted: true
-//     },
-//     {
-//         id: 2,
-//         todo: "Jog aound the park 3x",
-//         isCompleted: false
-//     },
-// ]
